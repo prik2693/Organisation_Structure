bb31d6127e6919192152dc424af1920bc6798dd1
Priyanshi Khatri <prik2693@users.noreply.github.com>
2017-01-22 02:31:09 +0530

    Update org.js

--- js/org.js
+++ js/org.js
@@ -1,43 +1,6 @@
 var app = angular.module('orgApp',['ngRoute','ngAnimate','ngMaterial']);
-//app.controller('indexToolbarCtrl',['$scope','$http','$rootScope','$location','UserDetails','$mdDialog',function($scope,$http,$rootScope,$location,UserDetails,$mdDialog){
-//    console.log("User Details ",UserDetails.getUserDetails());
-//    UserDetails.isAuthenticated.then(
-//        function(auth) {
-//            console.log("Header Frame resolved user name = "+ auth.userName);
-//            $scope.firstName= auth.firstName;
-//            $scope.lastName= auth.lastName;
-//            $rootScope.userId = auth.userId;
-//            $rootScope.userLevel = auth.userLevel;
-//        }
-//    );
-//    $scope.logout = function() {
-//        $rootScope.login=false;
-//        console.log("Try to logout");
-//        $http.post('/py/logout',{}).
-//        success(function(data, status, headers, config) {
-//                var user = {};
-//                user.userName = "";
-//                user.userLevel = -2;
-//                user.dept = "";
-//                location.reload();
-//                if (data.result == "OK") {
-//                    console.log("SUCCESS");
-//                    UserDetails.setDetail(user);
-//                    $location.path("/login");
-//                } else {
-//                    console.log("LOGOUT FAILED");
-//                }
-//        }).
-//        error(function(data, status, headers, config) {
-//            console.log("LOGOUT ERROR");
-//        });
-//     };
-//
-//    console.log("User Details new",UserDetails);
-//}]);
 app.controller('userCtrl', ['$scope', '$http', '$location','$rootScope','$mdDialog','$window','$mdToast',function($scope, $http, $location, $rootScope,$mdDialog,$window,$mdToast) {
-    //$rootScope.login = true;
-    $http.post('/py/getUserOrg',{}).
+        $http.post('/py/getUserOrg',{}).
         success(function(data, status, headers, config) {
                 if (data.status == "OK") {
                      $scope.user_org_struct = data.data;
